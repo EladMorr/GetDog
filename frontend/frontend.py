f540bd4107f1cb600a544f85628127be2cc7c944
EladMorr <eladmor95@gmail.com>
2022-12-26 23:59:36 +0200

    changes in fronend

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -4,10 +4,19 @@ import pandas as pd
 import mysql.connector
 from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
+from streamlit.components.v1 import html
 
 # st.markdown("https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-08/220805-border-collie-play-mn-1100-82d2f1.jpg")
 st.set_page_config(layout="wide")
-
+# st.markdown(
+#    f'''
+#    <style>
+#         p {
+#         background-image: url('https://img.freepik.com/free-photo/grunge-paint-background_1409-1337.jpg?w=2000');
+#         }
+#    </style>
+#    ''',
+#    unsafe_allow_html=True)
 
 st.title("Get a dog")
 st.write('''
@@ -27,35 +36,44 @@ dogs_db = pd.DataFrame({
     'phone number': ['0554453215', '0564443212'],
     'owner name': ["Elad", "Meital"],
     'price': [300, 680]
-})
+}, index=["1", "2"])
 
+dogs_db2 = dogs_db
 
-st.sidebar.header('Sidebar')
+menu = ["Home", "Add Dog", "Edit Dog", "Delete Dog", "About"]
+choice = st.sidebar.radio("Choose screen", menu)
 
 # Add content to the sidebar panel
 st.sidebar.text('This is a sidebar panel')
-
-# Add a button to the sidebar panel
-if st.sidebar.button('Click me'):
-    st.write('Button clicked!')
-
 
 st.sidebar.header('Panel 1')
 st.sidebar.text('This is the first sidebar panel')
 st.sidebar.slider(min_value=0, max_value=1000, label="Select maximum price:")
 
 # Create a second sidebar panel
-st.sidebar.header('Panel 2')
-st.sidebar.text('This is the second sidebar panel')
+st.sidebar.header('Edit Dog')
+st.sidebar.text('Edit the information about your dog')
+
+
+def add_dog(image, name, age, color, race, about, phoneNumber, ownerName, price):
+    df1 = pd.DataFrame({
+        "images": image,
+        "names": name
+    }, index=["3"])
+    dogs_db2 = dogs_db.append(df1)
+    st.dataframe(dogs_db2)
+    st.success("Great !!! your dog is added !")
+#     st.experimental_rerun()
 
 
 def get_dogs():
-    for index, row in dogs_db.iterrows():
+    st.dataframe(dogs_db2)
+    for index, row in dogs_db2.iterrows():
         st.image(row["images"],
                  width=400)
         st.write("""
-                Hello nice to meet tou :)
-                """)
+                                        Hello nice to meet tou :)
+                                        """)
         data = {'My Name': row["names"],
                 'My Age': row["ages"],
                 'My Color': row["colors"],
@@ -64,58 +82,54 @@ def get_dogs():
                 'Owner Number': row["phone number"],
                 'Owner Name': row["owner name"],
                 'price': row["price"]}
+
         df = pd.DataFrame(data, index=[0])
         df = df.reset_index(drop=True)
-        df.style.set_table_styles([{
-            'selector': 'th',
-            'props': [('background-color', '#FFA07A')]
-        }]).render()
         st.dataframe(df)
 
 
-# def get_data_from_db():
-#     cnx = mysql.connector.connect(
-#         host=127.0.0.1,
-#         user='root',
-#         password='',
-#         # database='pets'
-#     )
-#     cursor = cnx.cursor()
-#     query = "SELECT * FROM mytable"
-#     cursor.execute(query)
-#     for row in cursor:
-#         st.write(row)
-#     cursor.close()
-#     cnx.close()
+def add_bg_from_url():
+    st.markdown(
+        f"""
+         <style>
+         .stApp {{
+             background-image: url("https://media.istockphoto.com/id/1294440250/vector/seamless-gray-pattern-with-dog-paws-and-bones.jpg?s=612x612&w=0&k=20&c=nCTuqIEW5NHVvmsPuNShXRck0_EhzMskcnCY4qd70XI=");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+        unsafe_allow_html=True
+    )
 
 
-get_dogs()
+if choice == "Home":
+    get_dogs()
 
-# get_data_from_db()
+elif choice == "Add Dog":
+    st.header("Add Dog")
+    col1, col2 = st.columns(2)
+    with col1:
+        options = ["Take a picture", "Upload from computer", "URL"]
+        image_input = st.selectbox("Add a picture of your dog", options)
+        if image_input == "Take a picture":
+            st.camera_input("Take a picture")
+        elif image_input == "Upload from computer":
+            st.file_uploader("Upload from computer")
+        elif image_input == "URL":
+            st.text_input("Add image from URL")
+        name_input = st.text_input("Your dog name")
+        age_input = st.number_input("Your dog age")
+        color_input = st.text_input("Describe the color of your dog")
+        race_input = st.text_input("What is the race of your dog?")
+        about_input = st.text_area("Tell some about your dog...")
+        phone_number_input = st.text_input("Insert your phone number")
+        owner_name_input = st.text_input("Owner name")
+        price_input = st.text_input(
+            "What is your price? (it's better for free :))")
+        if st.button("Add"):
+            add_dog(image_input, name_input, age_input, color_input, race_input,
+                    about_input, phone_number_input, owner_name_input, price_input)
+        st.session_state = False
 
-
-# # Initialize connection.
-# # Uses st.experimental_singleton to only run once.
-# @st.experimental_singleton
-# def init_connection():
-#     return mysql.connector.connect(**st.secrets["mysql"])
-
-
-# conn = init_connection()
-
-# # Perform query.
-# # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-
-
-# @st.experimental_memo(ttl=600)
-# def run_query(query):
-#     with conn.cursor() as cur:
-#         cur.execute(query)
-#         return cur.fetchall()
-
-
-# rows = run_query("SELECT * from mytable;")
-
-# # Print results.
-# for row in rows:
-#     st.write(f"{row[0]} has a :{row[1]}:")
+add_bg_from_url()
